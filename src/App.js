418e4ede18220bb755a98ed0662fdebb7387ca31
Danilo Muñoz Silva <danilo.munoz.s@gmail.com>
2023-01-20 17:58:27 -0300

    actualizacion 7

--- src/App.js
+++ src/App.js
@@ -9,18 +9,24 @@ function App() {
 
   const [nombre, setNombre] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [error1, setError1] = useState(false);
+  const [error2, setError2] = useState(false);
   
   const validarDatos = (e) => {
       e.preventDefault();
 
-      if( nombre !='ADL'|| password !='252525')
+      if( nombre ===''|| password ==='')
     {
-      setError(true);
+      setError1(true);
       return;
     }  
-
-      setError(false);
+      else if( nombre !=='ADL'|| password !=='252525')
+    {
+      setError2(true);
+      setError1(false);
+      return;
+    } 
+      setError2(false);
       setNombre('');
       setPassword('');
     };
@@ -29,8 +35,8 @@ function App() {
  
   <div className='App-container'>
 
-    <Form className='formulario m-5 w-50' onSubmit={validarDatos}>
-      {error ? <p className='bg-danger text-white text-center'>Los datos son incorrectos!</p> : null}
+    <Form className='formulario m-5' onSubmit={validarDatos}>
+      {error1 ? <p className='bg-warning text-white text-center'>Debe ingresar datos!</p> : error2 ? <p className='bg-danger text-white text-center'>Los datos son incorrectos</p> : null}
         <Form.Label>Nombre</Form.Label>
         <Form.Control
         onChange={(e) => setNombre(e.target.value)}
